57c28f137cc9fc436c4cf6da151cb5c8d56127c1
sayjiep <akorepanova5@mail.ru>
2021-10-07 17:48:59 +0300

    create locations

---
+++ Place of games/LocationViewController/LocationTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LocationTableViewCell.swift
+//  Place of games
+//
+//  Created by Алёночка on 07.10.2021.
+//
+
+import UIKit
+
+class LocationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Place of games/Model/LocationModel.swift
@@ -0,0 +1,8 @@
+//
+//  LocationModel.swift
+//  Place of games
+//
+//  Created by Алёночка on 07.10.2021.
+//
+
+import Foundation
